37842ae2c09f1fc1c948fdebd53d03bc858c2dd9
Fhil Joshua <husphil200@gmail.com>
2024-06-12 18:16:27 +0800

    glimpse of hope

--- app/screens/mangaReader.jsx
+++ app/screens/mangaReader.jsx
@@ -1,143 +1,61 @@
-import { View, Text, Image, ActivityIndicator, Alert, ScrollView, 
-  Dimensions, Button, TouchableWithoutFeedback, StatusBar,
- } from 'react-native';
 import React, { useEffect, useState, useRef } from 'react';
-import { useLocalSearchParams } from 'expo-router';
+import { View, Text, ActivityIndicator, FlatList, Dimensions } from 'react-native';
 import shorthash from 'shorthash';
 import * as FileSystem from 'expo-file-system';
-import { getChapterImage, getChapterImageUrls, splitLongImage } from '../../utils/MangakakalotClient';
-import ModalPopup from '../../components/ModalPopup';
-import ImageWebView from "../../components/ImageWebView"
+import { getChapterImage, getChapterImageUrls } from '../../utils/MangakakalotClient';
 import ImageRenderer from '../../components/ImageRenderer';
 
 const MangaReaderScreen = () => {
-  const params = useLocalSearchParams();
-  const { chapterUrl, chData } = params;
-  const chapterData = JSON.parse(chData).map(chapter => chapter.chapterUrl);
-  const [chapterImages, setChapterImages] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [showModal, setShowModal] = useState(false);
-  const [cachedImageUris, setCachedImageUris] = useState([]);
-  const [currentChapterUrl, setCurrentChapterUrl] = useState(chapterUrl);
-  const [imgSlice, setImgSlice] = useState([])
-  const [pageUrls, setPageUrls] = useState([])
-
+  const [imgSlice, setImgSlice] = useState([]);
+  const screenWidth = Dimensions.get('window').width;
   const isMounted = useRef(true);
 
-  
-
-  const fetchData = async (url) => {
-    const imgSlice = await FileSystem.readAsStringAsync('file:///data/user/0/host.exp.exponent/cache/Z1Erv4L', { encoding: 'base64' });
-    setImgSlice(imgSlice)
-    
-    try {
-      //check cache - checking the cache for chaptserPagesUrl
-      const cacheKey = shorthash.unique(url)
-      const cachedChapterPageUris = `${FileSystem.cacheDirectory}${cacheKey}`
-      let pageUrls = []
-
-
-      
-      const fileInfo = await FileSystem.getInfoAsync(cachedChapterPageUris)
-
-      //if cache exist get that data if not request the data then cache the req data
-      if(fileInfo.exists) {
-        const cachedPageData = await FileSystem.readAsStringAsync(cachedChapterPageUris)
-        pageUrls = JSON.parse(cachedPageData)
-        }
-        else {
-        const requestedPageData = await getChapterImageUrls(url)
-        pageUrls = requestedPageData
-        await FileSystem.writeAsStringAsync(cachedChapterPageUris, JSON.stringify(pageUrls))
-      }
-
-      setPageUrls(pageUrls)
-      
-    } catch (error) {
-      alert(error.message)
-    } finally {
-      setIsLoading(false)
-    }
-
-  };
-
   useEffect(() => {
-    isMounted.current = true;
-    setIsLoading(true);
-    setChapterImages([]);
-    setCachedImageUris([]);
-    fetchData(currentChapterUrl);
+    const fetchData = async () => {
+      const imgSliceArr = [];
+      const imageUrls = [
+        'https://v7.mkklcdnv6tempv3.com/img/tab_27/02/91/17/dr980474/chapter_200/2-o.jpg',
+        'https://v7.mkklcdnv6tempv3.com/img/tab_27/02/91/17/dr980474/chapter_200/3-o.jpg'
+      ];
+      
+      for (const imageUrl of imageUrls) {
+        const imgSlice = await getChapterImage(imageUrl);
+        imgSliceArr.push(imgSlice);
+      }
+      
+      setImgSlice(imgSliceArr);
+      setIsLoading(false);
+    };
+
+    fetchData();
 
     return () => {
       isMounted.current = false;
     };
-  }, [currentChapterUrl]);
-
-  const clearCache = async () => {
-    try {
-      const cacheKey = shorthash.unique(currentChapterUrl);
-      const cachedImageUrlsFileUri = `${FileSystem.cacheDirectory}${cacheKey}`;
-      const fileInfo = await FileSystem.getInfoAsync(cachedImageUrlsFileUri);
-
-      if (fileInfo.exists) {
-        await FileSystem.deleteAsync(cachedImageUrlsFileUri);
-      }
-
-      for (const uri of cachedImageUris) {
-        const fileInfo = await FileSystem.getInfoAsync(uri);
-
-        if (fileInfo.exists) {
-          await FileSystem.deleteAsync(uri);
-        }
-      }
-
-      setChapterImages([]);
-      setCachedImageUris([]);
-      Alert.alert('Success', 'Cache cleared successfully');
-    } catch (error) {
-      Alert.alert('Failed to delete', "Cache already cleared");
-      console.log(error);
-    }
-  };
-
-  const handleShowModal = () => {
-    setShowModal(!showModal);
-  };
+  }, []);
 
   const handlePrevChap = () => {
-    const currentChapterIndex = chapterData.indexOf(currentChapterUrl);
-    if (currentChapterIndex < chapterData.length - 1) {
-      const prevChapterUrl = chapterData[currentChapterIndex + 1];
-      setCurrentChapterUrl(prevChapterUrl);
-    }
+    // Handle previous chapter logic
   };
 
   const handleNextChap = () => {
-    const currentChapterIndex = chapterData.indexOf(currentChapterUrl);
-    if (currentChapterIndex > 0) {
-      const nextChapterUrl = chapterData[currentChapterIndex - 1];
-      setCurrentChapterUrl(nextChapterUrl);
-    }
+    // Handle next chapter logic
   };
 
   return (
-    <View className="flex-1 bg-primary">
-      <StatusBar translucent />
-      <ModalPopup
-        visible={showModal}
-        onClose={handleShowModal}
-      >
-        <Text>Hello world</Text>
-        <Button title='Clear cache' onPress={clearCache} />
-        <Button title='Close' onPress={handleShowModal} />
-      </ModalPopup>
-      <View className="flex-1">
-        {isLoading && chapterImages.length === 0 ? (
-          <ActivityIndicator />
-        ) : (
-          <ImageWebView pageUrls={pageUrls}/>          
-        )}
-      </View>
+    <View style={{ flex: 1, backgroundColor: 'primary' }}>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <FlatList
+          data={imgSlice}
+          renderItem={({ item }) => <ImageRenderer imageData={item} screenWidth={screenWidth} />}
+          keyExtractor={(item, index) => index.toString()}
+          // horizontal
+          // pagingEnabled
+        />
+      )}
     </View>
   );
 };
--- components/ImageRenderer.jsx
+++ components/ImageRenderer.jsx
@@ -1,47 +1,80 @@
 import React, { useEffect, useState } from 'react';
 import { WebView } from 'react-native-webview';
+import { View, Image, Dimensions } from 'react-native';
 
 const ImageRenderer = ({ imageData }) => {
   const [htmlContent, setHtmlContent] = useState('');
+  const [imageHeight, setImageHeight] = useState(null);
+  const [imageWidth, setImageWidth] = useState(null);
 
   useEffect(() => {
     if (imageData) {
-      const content = `
-        <html>
-          <head>
-            <style>
-              body, html {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                height: 100%;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                overflow: hidden;
-              }
-              img {
-                width: 100%;
-                height: auto;
-              }
-            </style>
-          </head>
-          <body>
-            <img src="data:image/jpeg;base64,${imageData}" alt="Rendered Image" />
-          </body>
-        </html>
-      `;
-      setHtmlContent(content);
+      Image.getSize(`data:image/jpeg;base64,${imageData}`, (width, height) => {
+        const screenWidth = Dimensions.get('window').width;
+        const calculatedHeight = screenWidth / (width / height);
+
+        setImageWidth(screenWidth);
+        setImageHeight(calculatedHeight);
+
+        const content = `
+          <html>
+            <head>
+              <meta name="viewport" content="width=device-width, initial-scale=1.0">
+              <style>
+                body, html {
+                  margin: 0;
+                  padding: 0;
+                  width: 100%;
+                  height: 100%;
+                  display: flex;
+                  justify-content: center;
+                  align-items: center;
+                  overflow: hidden;
+                }
+                img {
+                  width: auto;
+                  height: auto;
+                  max-width: 100%;
+                  max-height: 100%;
+                  animation: fade-in 0.3s ease-out;
+                }
+                @keyframes fade-in {
+                  from { opacity: 0; }
+                  to { opacity: 1; }
+                }
+              </style>
+            </head>
+            <body>
+              <img id="renderedImage" src="data:image/jpeg;base64,${imageData}" alt="Rendered Image" />
+              <script>
+                const img = document.getElementById('renderedImage');
+                img.onload = function() {
+                  window.ReactNativeWebView.postMessage(JSON.stringify({ height: img.offsetHeight }));
+                }
+              </script>
+            </body>
+          </html>
+        `;
+        setHtmlContent(content);
+      });
     }
   }, [imageData]);
 
+  const onMessage = event => {
+    const { height } = JSON.parse(event.nativeEvent.data);
+    setImageHeight(height);
+  };
+
   return (
-    <WebView
-      originWhitelist={['*']}
-      source={{ html: htmlContent }}
-      style={{ flex: 1 }}
-      scrollEnabled={false}
-    />
+    <View style={{ height: imageHeight, width: imageWidth }}>
+      <WebView
+        originWhitelist={['*']}
+        source={{ html: htmlContent }}
+        style={{ height: imageHeight }}
+        scrollEnabled={false}
+        onMessage={onMessage}
+      />
+    </View>
   );
 };
 
