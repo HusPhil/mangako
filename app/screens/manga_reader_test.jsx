2b3a1caaca99b214a92d2ae59bd778544a8e9b09
HusPhil <husphil200@gmail.com>
2024-07-10 22:34:15 +0800

    redux implemented, reader screen code refactored

--- app/screens/manga_reader_test.jsx
+++ app/screens/manga_reader_test.jsx
@@ -1,5 +1,5 @@
-import { View, Text, Button } from 'react-native'
-import React, {useState, useRef, useEffect, useCallback } from 'react'
+import { View, Text } from 'react-native'
+import React, {useRef, useEffect, useReducer } from 'react'
 import { useLocalSearchParams } from 'expo-router';
 
 import * as backend from "./_manga_reader"
@@ -9,31 +9,28 @@ import DropDownList from '../../components/modal/DropdownList';
 import ModalPopup from '../../components/modal/ModalPopup';
 import HorizontalRule from '../../components/HorizontalRule';
 
+import { readerReducer, INITIAL_STATE } from '../../redux/readerScreen/readerReducer';
+import { READER_ACTIONS } from '../../redux/readerScreen/readerActions';
 
 const MangaReaderScreen = () => {
     const {mangaUrl, currentChapterData } = useLocalSearchParams()
-    
-    const [chapterPages, setChapterPages] = useState([])
-    const [isLoading, setIsLoading] = useState(false)
-    const [errorData, setErrorData] = useState(null)
+    const parsedCurrentChapterData = JSON.parse(currentChapterData)
 
-    const [showModal, setShowModal] = useState(false)
-    const [readingMode, setReadingMode] = useState(backend.readerModeOptions['2'])
+    const [state, dispatch] = useReducer(readerReducer, INITIAL_STATE)
 
     const isMounted = useRef(true)
     const controllerRef = useRef(null)
-    const parsedCurrentChapterData = useRef(JSON.parse(currentChapterData))
 
     const AsyncEffect = async () => {
         
-        setIsLoading(true);
-
+        dispatch({type: READER_ACTIONS.GET_CHAPTER_PAGES})
         controllerRef.current = new AbortController();
         const signal = controllerRef.current.signal;
 
         try {
-            const fetchedChapterPages = await backend.fetchData(mangaUrl, parsedCurrentChapterData.current.chapterUrl, signal);
-            setChapterPages(fetchedChapterPages.data);
+            const fetchedChapterPages = await backend.fetchData(mangaUrl, parsedCurrentChapterData.chapterUrl, signal);
+            if(fetchedChapterPages.error) throw fetchedChapterPages.error
+            dispatch({type: READER_ACTIONS.GET_CHAPTER_PAGES_SUCCESS, payload: fetchedChapterPages.data})
         } 
         catch (error) {
             if (signal.aborted) {
@@ -41,12 +38,9 @@ const MangaReaderScreen = () => {
             } else {
                 console.log("Error fetching chapter pages:", error);
             }
-            setChapterPages([])
-            setErrorData(error)
+            dispatch({type: READER_ACTIONS.GET_CHAPTER_PAGES_ERROR})
         } 
-        finally {
-            setIsLoading(false);
-        }
+        
     }
 
     useEffect(() => {
@@ -54,20 +48,16 @@ const MangaReaderScreen = () => {
         return () => {
             isMounted.current = false
             controllerRef.current.abort()
-            setChapterPages([])
+            dispatch({type: READER_ACTIONS.EFFECT_CLEAN_UP})
         }
     }, [])
 
-    const handleShowModal = useCallback(() => {
-        setShowModal(prev => !prev);
-      }, []);
-
     return (
         <View className="h-full bg-primary">
-            <ModalPopup visible={showModal} handleClose={handleShowModal}>
+            <ModalPopup visible={state.showModal} handleClose={() => {dispatch({type: READER_ACTIONS.SHOW_MODAL, payload: state.showModal})}}>
                 <View className="mx-4">
                     <View className="justify-start w-full">
-                    <Text numberOfLines={1} className="text-white font-pregular text-base text-center p-2 py-3">{parsedCurrentChapterData.current.chTitle}</Text>
+                    <Text numberOfLines={1} className="text-white font-pregular text-base text-center p-2 py-3">{parsedCurrentChapterData.chTitle}</Text>
                     </View>
                     <HorizontalRule />
                     <View className="w-full">
@@ -77,46 +67,52 @@ const MangaReaderScreen = () => {
                         details={"hello world"}
                         listItems={backend.readerModeOptions}
                         onValueChange={(data) => {
-                          setReadingMode(data);
+                          dispatch({type: READER_ACTIONS.SET_READER_MODE, payload: data})
                         }}
-                        selectedIndex={backend.readerModeOptions.indexOf(readingMode)}
+                        selectedIndex={backend.readerModeOptions.indexOf(state.readingMode)}
                     />
                     </View>
                 </View>
           </ModalPopup>
-            {!isLoading && (
+            {!state.isLoading && (
                 <>
-                {readingMode === backend.readerModeOptions[0] && (
+                {state.readingMode === backend.readerModeOptions[0] && (
                     <HorizontalReader 
-                        chapterPages={chapterPages}
+                        chapterPages={state.chapterPages}
                         currentManga={{
                             manga: mangaUrl,
-                            chapter: parsedCurrentChapterData.current.chapterUrl
+                            chapter: parsedCurrentChapterData.chapterUrl
                         }}
-                        onTap={handleShowModal}
+                        onTap={() => {
+                            dispatch({type: READER_ACTIONS.SHOW_MODAL, payload: state.showModal})
+                        }}
                     />
                 )}
 
-                {readingMode === backend.readerModeOptions[1] && (
+                {state.readingMode === backend.readerModeOptions[1] && (
                     <HorizontalReader 
-                        chapterPages={chapterPages}
+                        chapterPages={state.chapterPages}
                         currentManga={{
                             manga: mangaUrl,
-                            chapter: parsedCurrentChapterData.current.chapterUrl
+                            chapter: parsedCurrentChapterData.chapterUrl
                         }}
-                        onTap={handleShowModal}
+                        onTap={() => {
+                            dispatch({type: READER_ACTIONS.SHOW_MODAL, payload: state.showModal})
+                        }}
                         inverted
                     />
                 )}
 
-                {readingMode === backend.readerModeOptions[2] && (
+                {state.readingMode === backend.readerModeOptions[2] && (
                     <VerticalReader 
-                        chapterPages={chapterPages}
+                        chapterPages={state.chapterPages}
                         currentManga={{
                             manga: mangaUrl,
-                            chapter: parsedCurrentChapterData.current.chapterUrl
+                            chapter: parsedCurrentChapterData.chapterUrl
                         }}
-                        onTap={handleShowModal}
+                        onTap={() => {
+                            dispatch({type: READER_ACTIONS.SHOW_MODAL, payload: state.showModal})
+                        }}
                         inverted
                     />
                 )}
---
+++ redux/readerScreen/readerReducer.js
@@ -0,0 +1,46 @@
+import { readerModeOptions } from "../../app/screens/_manga_reader"
+import { READER_ACTIONS } from "./readerActions"
+
+export const INITIAL_STATE = {
+    chapterPages: [],
+    isLoading: false,
+    errorData: null,
+    showModal: false,
+    readingMode: readerModeOptions['0'],
+}
+
+export const readerReducer = (state, action) => {
+    console.log(action.type)
+    switch (action.type) {
+        case READER_ACTIONS.GET_CHAPTER_PAGES:
+            return {
+                ...state,
+                isLoading: true,
+            };
+        case READER_ACTIONS.GET_CHAPTER_PAGES_SUCCESS:
+            return {
+                ...state,
+                chapterPages: action.payload,
+                isLoading: false,
+            };
+        case READER_ACTIONS.GET_CHAPTER_PAGES_ERROR:
+            return {
+                ...state,
+                error: action.payload,
+            };
+        case READER_ACTIONS.SET_READER_MODE:
+            return {
+                ...state,
+                readingMode: action.payload,
+            };
+        case READER_ACTIONS.SHOW_MODAL:
+            return {
+                ...state,
+                showModal: !action.payload
+            };
+        case READER_ACTIONS.EFFECT_CLEAN_UP:
+            return INITIAL_STATE;
+        default:
+            return state;
+    }
+}
