99312ff4d9396bf490f60e027afd8be47f118ac7
obsidian-sync <husphil200@gmail.com>
2026-03-28 14:30:54 +0800

    lag issue

--- app/manga/[id]/[chapterId].tsx
+++ app/manga/[id]/[chapterId].tsx
@@ -1,84 +1,41 @@
-import { useLibraryStore } from "@/stores/library-store";
-import { useReadingStore } from "@/stores/reading-progress-store";
-import { Button } from "@react-navigation/elements";
-import { useLocalSearchParams } from "expo-router";
-import React, { useCallback, useMemo } from "react";
-import { Text, View } from "react-native";
+import MangaReaderPage from "@/components/manga-reader-screen-components/MangaReaderPage";
+import { Colors } from "@/constants/colors";
+import { useMangaReaderScreenLogic } from "@/hooks/manga-reader-screen-hooks/useMangaReaderScreenLogic";
+import { MangaChapterPage } from "@/types/ResponseTypes";
+import { FlashList } from "@shopify/flash-list";
+import React, { useCallback } from "react";
+import { ActivityIndicator, StatusBar, View } from "react-native";
 
 const MangaReaderScreen = () => {
-  const {
-    id: mangaId,
-    chapterId,
-    chapterTitle,
-    chapterUrl,
-  } = useLocalSearchParams();
+  const { pages, isLoading, isError, getItemLayout, onBack } =
+    useMangaReaderScreenLogic();
 
-  if (!mangaId) return null;
-
-  const getMangaById = useLibraryStore((state) => state.getMangaById);
-  const mangaInfo = useMemo(() => getMangaById(mangaId as string), [mangaId]);
-
-  const mangaUrl = mangaInfo?.manga_url;
-  const mangaCover = mangaInfo?.cover_url;
-  const mangaTitle = mangaInfo?.title;
-  const mangaSourceId = mangaInfo?.source_id;
-
-  if (!mangaTitle || !mangaCover || !mangaUrl || !mangaSourceId || !mangaId)
-    return null;
-
-  const handleMarkAsRead = useCallback((mangaId: string, chapterId: string) => {
-    const isRead = true;
-    const mangaInput = useLibraryStore.getState().getMangaById(mangaId);
-
-    useReadingStore.getState().markChapters(
-      mangaId,
-      [
-        {
-          id: chapterId,
-          title: chapterTitle as string,
-          url: chapterUrl as string,
-        },
-      ],
-      isRead,
-    );
-  }, []);
-
-  const handleMarkAsUnRead = useCallback(
-    (mangaId: string, chapterId: string) => {
-      const isRead = false;
-      useReadingStore.getState().markChapters(
-        mangaId,
-        [
-          {
-            id: chapterId,
-            title: chapterTitle as string,
-            url: chapterUrl as string,
-          },
-        ],
-        isRead,
-      );
-    },
+  const renderItem = useCallback(
+    ({ item }: { item: MangaChapterPage }) => <MangaReaderPage item={item} />,
     [],
   );
 
+  if (isLoading) {
+    return (
+      <View className="flex-1 bg-background justify-center items-center">
+        <ActivityIndicator color={Colors.primary} size="large" />
+      </View>
+    );
+  }
+
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="bg-primary p-5">{mangaId}</Text>
-      <Text className="bg-primary p-5">{chapterId}</Text>
-      <Button
-        onPressOut={() => {
-          handleMarkAsRead(mangaId as string, chapterId as string);
-        }}
-      >
-        Mark as read
-      </Button>
-      <Button
-        onPressOut={() => {
-          handleMarkAsUnRead(mangaId as string, chapterId as string);
-        }}
-      >
-        Mark as unread
-      </Button>
+    <View className="flex-1 bg-black">
+      <StatusBar hidden />
+
+      <FlashList
+        data={pages}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.pageId}
+        removeClippedSubviews={true} // Physically destroy off-screen views
+        drawDistance={500} // Only render 500px above/below the screen
+      />
+
+      {/* We will add Overlays (Header/Footer) here next */}
     </View>
   );
 };
