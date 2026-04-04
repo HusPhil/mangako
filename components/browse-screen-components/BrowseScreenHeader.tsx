03a3a37e5e6c6f14499c3358e8c51d9dcf2f4165
HusPhil009 <husphil200@gmail.com>
2026-04-04 11:55:52 +0800

    ui for search and source

--- components/browse-screen-components/BrowseScreenHeader.tsx
+++ components/browse-screen-components/BrowseScreenHeader.tsx
@@ -1,7 +1,8 @@
 import { Colors } from "@/constants/colors";
+import { AntDesign, Ionicons, Octicons } from "@expo/vector-icons";
 import { Image } from "expo-image";
 import React from "react";
-import { StatusBar, Text, View } from "react-native";
+import { Pressable, StatusBar, Text, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 const ramenIcon = require("@/assets/images/ramen_mini_icon.png");
 
@@ -10,16 +11,60 @@ const BrowseScreenHeader = () => {
   return (
     <View className="w-full bg-background" style={{ paddingTop: insets.top }}>
       <StatusBar barStyle="light-content" backgroundColor={Colors.background} />
-      <View className="flex-row items-center p-5 gap-3">
-        <Image
-          source={ramenIcon}
-          style={{
-            height: "undefined" as any,
-            width: 35,
-            aspectRatio: 1,
-          }}
-        />
-        <Text className="text-primary text-3xl font-bold">Browse</Text>
+      <View className="flex-row items-center p-5 justify-between  ">
+        <View className="flex-row items-center gap-3">
+          <Image
+            source={ramenIcon}
+            style={{
+              height: "undefined" as any,
+              width: 35,
+              aspectRatio: 1,
+            }}
+          />
+          <Text className="text-primary text-3xl font-bold">Browse</Text>
+        </View>
+        {/* Source Switcher */}
+        <Pressable
+          style={({ pressed }) => [
+            {
+              opacity: pressed ? 0.8 : 1,
+              transform: [{ scale: pressed ? 0.96 : 1 }],
+            },
+          ]}
+          className="flex-row items-center bg-white/10 px-3 py-2 rounded-xl border border-white/5"
+        >
+          <Text className="text-white/80 text-xs font-bold uppercase  tracking-widest">
+            WEEB CENTRAL
+          </Text>
+          {/* vertical separator */}
+          <View className="border-l border-white/10 h-4 mx-2" />
+          <AntDesign name="swap" size={16} color={Colors.primary} />
+        </Pressable>
+      </View>
+      {/* Improved Search Section */}
+      <View className="px-5 mb-5">
+        <Pressable
+          className="flex-row items-center bg-white/5 border border-white/10 py-2 px-4 rounded-xl"
+          style={({ pressed }) => [{ opacity: pressed ? 0.7 : 1 }]}
+        >
+          <Octicons
+            name="triangle-right"
+            size={30}
+            style={{ marginLeft: -8 }}
+            color="#A0A0A0"
+          />
+
+          <View className="flex-1">
+            <Text className="text-white/50 text-base font-medium">
+              Search your favorite manga...
+            </Text>
+          </View>
+
+          {/* Filter Icon - Useful for UX */}
+          <View className="pl-3 border-l border-white/10">
+            <Ionicons name="search" size={20} color={Colors.primary} />
+          </View>
+        </Pressable>
       </View>
     </View>
   );
